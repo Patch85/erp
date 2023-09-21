ac8de70b57001ff87ef4db798764accb5ccf64d0
Dillon A. John <dillon.a.john@gmail.com>
2023-09-21 08:56:53 -0400

    0.2.3 - Users: Tests
    - Update Feature/Auth/ test files for AUthentication, Password
      Confirmation, Password Updating, and Registration to account for
      changes to the User model
    - Update Feature/Profile test to account for the same

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -28,7 +28,7 @@ class ProfileTest extends TestCase
         $response = $this
             ->actingAs($user)
             ->patch('/profile', [
-                'name' => 'Test User',
+                'first_name' => 'Test',
                 'email' => 'test@example.com',
             ]);
 
@@ -38,7 +38,7 @@ class ProfileTest extends TestCase
 
         $user->refresh();
 
-        $this->assertSame('Test User', $user->name);
+        $this->assertSame('Test', $user->first_name);
         $this->assertSame('test@example.com', $user->email);
         $this->assertNull($user->email_verified_at);
     }
@@ -50,7 +50,7 @@ class ProfileTest extends TestCase
         $response = $this
             ->actingAs($user)
             ->patch('/profile', [
-                'name' => 'Test User',
+                'first_name' => 'Test',
                 'email' => $user->email,
             ]);
 
@@ -63,7 +63,7 @@ class ProfileTest extends TestCase
 
     public function test_user_can_delete_their_account(): void
     {
-        $user = User::factory()->create();
+        $user = User::factory()->create(['password' => 'password']);
 
         $response = $this
             ->actingAs($user)
