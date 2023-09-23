adf2637b7967568673e2ce8e2db0bb07772163bb
Dillon A. John <dillon.a.john@gmail.com>
2023-09-23 15:28:40 -0400

    0.2.6 - Theming: Layouts & Login
    - Updated tailwind colors in GuestLayout, AuthenticatedLayout, TextInput
      and Login to match the theming changes in Welcome and the new
      ApplicationLogo

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -22,10 +22,8 @@ defineExpose({ focus: () => input.value.focus() });
 </script>
 
 <template>
-    <input
-        class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
-        :value="modelValue"
-        @input="$emit('update:modelValue', $event.target.value)"
-        ref="input"
-    />
+    <input class="border-gray-300 rounded-md shadow-sm dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-blue-500 dark:focus:border-sky-300 focus:ring-blue-500 dark:focus:ring-sky-300"
+           :value="modelValue"
+           @input="$emit('update:modelValue', $event.target.value)"
+           ref="input" />
 </template>
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -31,64 +31,66 @@ const submit = () => {
 
 <template>
     <GuestLayout>
-        <Head title="Log in" />
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+        <Head title="Log In" />
+
+        <div v-if="status"
+             class="mb-4 text-sm font-medium text-green-600">
             {{ status }}
         </div>
 
         <form @submit.prevent="submit">
             <div>
-                <InputLabel for="email" value="Email" />
+                <InputLabel for="email"
+                            value="Email" />
 
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
+                <TextInput id="email"
+                           type="email"
+                           class="block w-full mt-1"
+                           v-model="form.email"
+                           required
+                           autofocus
+                           autocomplete="username" />
 
-                <InputError class="mt-2" :message="form.errors.email" />
+                <InputError class="mt-2"
+                            :message="form.errors.email" />
             </div>
 
             <div class="mt-4">
-                <InputLabel for="password" value="Password" />
+                <InputLabel for="password"
+                            value="Password" />
 
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="current-password"
-                />
+                <TextInput id="password"
+                           type="password"
+                           class="block w-full mt-1"
+                           v-model="form.password"
+                           required
+                           autocomplete="current-password" />
 
-                <InputError class="mt-2" :message="form.errors.password" />
+                <InputError class="mt-2"
+                            :message="form.errors.password" />
             </div>
 
             <div class="block mt-4">
                 <label class="flex items-center">
-                    <Checkbox name="remember" v-model:checked="form.remember" />
+                    <Checkbox name="remember"
+                              v-model:checked="form.remember" />
                     <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">Remember me</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <Link
-                    v-if="canResetPassword"
-                    :href="route('password.request')"
-                    class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                >
-                    Forgot your password?
+                <Link v-if="canResetPassword"
+                      :href="route('password.request')"
+                      class="text-sm text-gray-600 underline rounded-md dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:focus:ring-sky-300 dark:focus:ring-offset-gray-800">
+                Forgot your password?
                 </Link>
 
-                <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                <PrimaryButton class="ml-4"
+                               :class="{ 'opacity-25': form.processing }"
+                               :disabled="form.processing">
                     Log in
                 </PrimaryButton>
             </div>
         </form>
-    </GuestLayout>
-</template>
+    </GuestLayout></template>
