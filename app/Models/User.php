68e5599e5a441cc4484d85ef51ee0a61c0a82aff
Dillon A. John <dillon.a.john@gmail.com>
2024-02-10 14:38:05 -0500

    0.3.3 - Addresses: User Address
    - Add new User::address() method to return a  MorphOne relationship
      to the Address model.
    - A user can have one address.
    - Update AddressTest::test_addressable_relationship to use the new
      User::address() method to create a new address for the user.

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -45,6 +46,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function address(): MorphOne
+    {
+        return $this->morphOne(Address::class, 'addressable');
+    }
+
     public function fullName(): string
     {
         return match (true) {
