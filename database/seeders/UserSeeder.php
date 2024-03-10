b5c0f8f11002a6b2c7a543598ce73f1259b7028e
Dillon A. John <dillon.a.john@gmail.com>
2024-03-10 16:04:36 -0400

    0.3.6 - Addresses: User Seeder - User Addresses
    - Updated UserSeeder to create a User Address for each User created by
      the factory.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Address;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -13,7 +15,7 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         if (config('app.env') == 'local') {
-            \App\Models\User::factory()->create([
+            User::factory()->create([
                 'first_name' => config('dev.first_name'),
                 'middle_name' => config('dev.middle_name'),
                 'last_name' => config('dev.last_name'),
@@ -23,7 +25,17 @@ class UserSeeder extends Seeder
             ]);
         }
 
-        \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
 
+        User::all()->each(function ($user) {
+            $user->address()->save(Address::factory()->make(
+                [
+                    'addressable_type' => User::class,
+                    'addressable_id' => $user->id,
+                    'attention' => $user->fullName(),
+                    'description' => 'Home Address',
+                ]
+            ));
+        });
     }
 }
