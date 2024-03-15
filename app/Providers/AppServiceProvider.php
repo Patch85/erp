7a020bea54b6fee6f0942c3dda848c1dfb966626
Dillon John <dillon.john@techflex.com>
2024-03-15 16:57:04 -0400

    0.4.1 - Blameable - Blueprint::blameable() macro
    - Added Blueprint::blameable() macro to AppServiceProvider.php
      This allows adding created_by and updated_by columns to our
      migrations with a single line of code.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Blueprint::macro('blameable', function () {
+            $this->string('created_by');
+            $this->string('updated_by');
+        });
     }
 }
